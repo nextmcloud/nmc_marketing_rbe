a59069c73926fcb3a71c15e31a88a490901becca
T-Systems, Digital Architecture Solutions with Cloud <31852409+tsdicloud@users.noreply.github.com>
2023-03-30 19:58:43 +0200

    Delete nmc_marketing.config.php

--- nmc_marketing.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$CONFIG = [
-  'nmc_marketing'=>array(
-    'script_url' => 'https://tags-eu.tiqcdn.com/utag/telekom/mediencenter/prod/utag.js',
-    'font_url' => 'https://ebs10.telekom.de'
-  )
-];
